f78e00c1f78235881e9bf03d4ced96620176703a
hyunsu jang <jhsnex3768@gmail.com>
2024-12-01 15:18:49 +0900

    first commit

---
+++ src/test/java/com/kosa/moimeasy/MoimeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.moimeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
