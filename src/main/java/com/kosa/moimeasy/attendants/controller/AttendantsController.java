2a7e56ad31616baed9e3c74207d520a2fab8578a
JiHun Kim <dydekffrl@naver.com>
2024-12-08 04:37:50 +0900

    feat : 요청,일정,알림 컨트롤러 서비스 추가

---
+++ src/main/java/com/kosa/moimeasy/attendants/controller/AttendantsController.java
@@ -0,0 +1,7 @@
+package com.kosa.moimeasy.attendants.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AttendantsController {
+}
--- src/main/java/com/kosa/moimeasy/board/service/BoardService.java
+++ src/main/java/com/kosa/moimeasy/board/service/BoardService.java
@@ -1,4 +1,7 @@
 package com.kosa.moimeasy.board.service;
 
-public interface BoardService {
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
 }
