6688f3e44ebef7d11f3bbc40ee69e31ac6250291
JiHun Kim <dydekffrl@naver.com>
2024-12-14 14:40:32 +0900

    refactor : 리팩토링

--- src/main/java/com/kosa/moimeasy/user/entity/User.java
+++ src/main/java/com/kosa/moimeasy/user/entity/User.java
@@ -35,60 +35,22 @@ public class User {
     @Column(length = 20)
     private String phone;
 
-    @Column(name = "create_at", nullable = false, updatable = false)
-    private LocalDateTime createAt;
-
-    @Column(name = "update_at")
-    private LocalDateTime updateAt;
-
     @ManyToOne(fetch = FetchType.LAZY) // Role과의 관계 설정
     @JoinColumn(name = "role_id", nullable = false) // 외래 키 매핑
     private Role role;
 
-    @Column(length = 25)
+    @Column(length = 25, nullable = true)
     private String nickname;
 
+    @Column(length = 255)
+    private String profileImage;
+
     @Column(nullable = true)
-    private Long moeimId;
-
-    @PrePersist
-    public void prePersist() {
-        this.createAt = LocalDateTime.now();
-        if(this.createAt == null){
-            this.createAt = LocalDateTime.now();
-        }else {
-            this.createAt = createAt;
-        }
-
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updateAt = LocalDateTime.now();
-    }
+    private Long moeimI;
 
     @OneToMany(mappedBy = "user")
     private List<UserAccount> userAccounts = new ArrayList<>();
 }
-
-
-//    public enum Role {
-//        user,admin
-//    }
-
-
-
-//    @Column(name = "PROFILE_URL")
-//    private String profileUrl;
-//
-//    // 다대다 관계 설정
-//    @ManyToMany
-//    @JoinTable(
-//            name = "USER_MOEIM",
-//            joinColumns = @JoinColumn(name = "USER_ID"),
-//            inverseJoinColumns = @JoinColumn(name = "MOEIM_ID")
-//    )
-//    private Set<Moeim> moeims = new HashSet<>();
 
 
 
