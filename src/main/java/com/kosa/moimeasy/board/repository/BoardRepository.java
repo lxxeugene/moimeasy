9d591a58162a0173eb305089e48f6bee1de7bc1f
JiHun Kim <dydekffrl@naver.com>
2024-12-17 01:54:20 +0900

    feat : 모임 게시판 조회

--- src/main/java/com/kosa/moimeasy/board/repository/BoardRepository.java
+++ src/main/java/com/kosa/moimeasy/board/repository/BoardRepository.java
@@ -1,9 +1,18 @@
 package com.kosa.moimeasy.board.repository;
 
+import com.kosa.moimeasy.board.dto.BoardDTO;
 import com.kosa.moimeasy.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("SELECT new com.kosa.moimeasy.board.dto.BoardDTO(b.boardId, b.title, b.content, b.isNotice, u.userName, u.profileImage ,b.tag, b.createAt,b.updateAt) " +
+            "FROM Board b " +
+            "JOIN b.writer u "+ // Board와 User를 조인
+            "ORDER BY b.boardId DESC")
+    List<BoardDTO> findAllBoardsWithWriterName();
 }
--- src/main/java/com/kosa/moimeasy/board/service/BoardService.java
+++ src/main/java/com/kosa/moimeasy/board/service/BoardService.java
@@ -1,7 +1,55 @@
 package com.kosa.moimeasy.board.service;
 
+import com.kosa.moimeasy.board.dto.BoardDTO;
+import com.kosa.moimeasy.board.entity.Board;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import com.kosa.moimeasy.board.repository.BoardRepository;
+import com.kosa.moimeasy.user.entity.User;
+import com.kosa.moimeasy.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+
+    // 모든 게시글 조회
+    public List<BoardDTO> getAllBoards() {
+        return boardRepository.findAllBoardsWithWriterName();
+    }
+
+    public Board getBoardById(Long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + id));
+    }
+
+    // 게시글 추가
+    public Board createBoard(BoardDTO boardDTO,Long userId) {
+
+        User writer = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("작성자를 찾을 수 없습니다."));
+        Board board=boardDTO.toEntity(writer);
+        return boardRepository.save(board);
+    }
+
+    // 게시글 수정
+    public Board updateBoard(Long id, Board boardDetails) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + id));
+
+        board.setTitle(boardDetails.getTitle());
+        board.setContent(boardDetails.getContent());
+        board.setIsNotice(boardDetails.getIsNotice());
+        return boardRepository.save(board);
+    }
+
+    //게시글 삭제하기
+    public void deleteBoard(Long id) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + id));
+        boardRepository.delete(board);
+    }
 }
